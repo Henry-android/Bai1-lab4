709d4fcaaa02c9b46567108e86873078c0bfb6b2
Henry-android <khanhphan.040204@gmail.com>
2024-12-12 10:05:43 +0700

    Bai Tap Buoi 6

---
+++ BaiTH4/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace BaiTH4
+{
+    partial class frmSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvSV = new System.Windows.Forms.DataGridView();
+            this.colmssv = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colHoTen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colKhoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDiemTB = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmssv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDiemTb = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.cmbKhoa = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(390, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quản lý thông tin sinh viên ";
+            // 
+            // dgvSV
+            // 
+            this.dgvSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colmssv,
+            this.colHoTen,
+            this.colKhoa,
+            this.colDiemTB});
+            this.dgvSV.Location = new System.Drawing.Point(349, 107);
+            this.dgvSV.Name = "dgvSV";
+            this.dgvSV.RowHeadersWidth = 51;
+            this.dgvSV.RowTemplate.Height = 24;
+            this.dgvSV.Size = new System.Drawing.Size(553, 319);
+            this.dgvSV.TabIndex = 1;
+            this.dgvSV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSV_CellClick);
+            // 
+            // colmssv
+            // 
+            this.colmssv.HeaderText = "Mã Số SV";
+            this.colmssv.MinimumWidth = 6;
+            this.colmssv.Name = "colmssv";
+            this.colmssv.Width = 125;
+            // 
+            // colHoTen
+            // 
+            this.colHoTen.HeaderText = "Họ Tên";
+            this.colHoTen.MinimumWidth = 6;
+            this.colHoTen.Name = "colHoTen";
+            this.colHoTen.Width = 125;
+            // 
+            // colKhoa
+            // 
+            this.colKhoa.HeaderText = "tên Khoa";
+            this.colKhoa.MinimumWidth = 6;
+            this.colKhoa.Name = "colKhoa";
+            this.colKhoa.Width = 125;
+            // 
+            // colDiemTB
+            // 
+            this.colDiemTB.HeaderText = "Điểm TB";
+            this.colDiemTB.MinimumWidth = 6;
+            this.colDiemTB.Name = "colDiemTB";
+            this.colDiemTB.Width = 125;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã số Sv";
+            // 
+            // txtmssv
+            // 
+            this.txtmssv.Location = new System.Drawing.Point(138, 85);
+            this.txtmssv.Name = "txtmssv";
+            this.txtmssv.Size = new System.Drawing.Size(173, 22);
+            this.txtmssv.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ Tên";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(138, 146);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(173, 22);
+            this.txtHoTen.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Khoa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Điểm TB";
+            // 
+            // txtDiemTb
+            // 
+            this.txtDiemTb.Location = new System.Drawing.Point(138, 283);
+            this.txtDiemTb.Name = "txtDiemTb";
+            this.txtDiemTb.Size = new System.Drawing.Size(173, 22);
+            this.txtDiemTb.TabIndex = 3;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(32, 390);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 4;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(152, 390);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 4;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(253, 390);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 4;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // cmbKhoa
+            // 
+            this.cmbKhoa.FormattingEnabled = true;
+            this.cmbKhoa.Items.AddRange(new object[] {
+            "Công nghệ thông tin",
+            "Ngôn ngữ Anh ",
+            "Quản trị kinh doanh"});
+            this.cmbKhoa.Location = new System.Drawing.Point(138, 212);
+            this.cmbKhoa.Name = "cmbKhoa";
+            this.cmbKhoa.Size = new System.Drawing.Size(173, 24);
+            this.cmbKhoa.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(69, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Thông tin sinh viên";
+            // 
+            // frmSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 535);
+            this.Controls.Add(this.cmbKhoa);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtDiemTb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtmssv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvSV);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSV";
+            this.Text = "Quản lý Sinh Viên";
+            this.Load += new System.EventHandler(this.frmSV_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvSV;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtmssv;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDiemTb;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.ComboBox cmbKhoa;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colmssv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colHoTen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colKhoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDiemTB;
+    }
+}
+
---
+++ BaiTH4/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTH4
+{
+    public partial class frmSV : Form
+    {
+        public frmSV()
+        {
+            InitializeComponent();
+        }
+
+        private void frmSV_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Model1 context = new Model1();
+                List<Faculty> listFalcultys = context.Faculty.ToList(); //lấy các khoa
+                List<Student> listStudent = context.Student.ToList(); //lấy sinh viên
+                FillFalcultyCombobox(listFalcultys);
+                BindGrid(listStudent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void FillFalcultyCombobox(List<Faculty> listFalcultys)
+        {
+            this.cmbKhoa.DataSource = listFalcultys;
+            this.cmbKhoa.DisplayMember = "FacultyName";
+            this.cmbKhoa.ValueMember = "FacultyID";
+        }
+
+        private void BindGrid(List<Student> listStudent)
+        {
+            dgvSV.Rows.Clear();
+            foreach (var item in listStudent)
+            {
+                int index = dgvSV.Rows.Add();
+                dgvSV.Rows[index].Cells[0].Value = item.StudentID;
+                dgvSV.Rows[index].Cells[1].Value = item.FullName;
+                dgvSV.Rows[index].Cells[2].Value = item.Faculty.FacultyName;
+                dgvSV.Rows[index].Cells[3].Value = item.AverageScore;
+            }
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Model1 db = new Model1())
+                {
+                    string studentIDText = txtmssv.Text.Trim();
+                    string fullName = txtHoTen.Text.Trim();
+                    string averageScoreText = txtDiemTb.Text.Trim();
+
+                    if (string.IsNullOrEmpty(studentIDText))
+                    {
+                        MessageBox.Show("Mã sinh viên không được để trống.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!int.TryParse(studentIDText, out int studentID))
+                    {
+                        MessageBox.Show("Mã sinh viên phải là một số nguyên.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (string.IsNullOrEmpty(fullName))
+                    {
+                        MessageBox.Show("Tên sinh viên không được để trống.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!int.TryParse(cmbKhoa.SelectedValue?.ToString(), out int facultyID))
+                    {
+                        MessageBox.Show("Vui lòng chọn một khoa hợp lệ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!double.TryParse(averageScoreText, out double averageScore))
+                    {
+                        MessageBox.Show("Điểm trung bình không hợp lệ. Vui lòng nhập số.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    var newStudent = new Student
+                    {
+                        StudentID = studentID, // Gán kiểu int
+                        FullName = fullName,
+                        FacultyID = facultyID,
+                        AverageScore = averageScore
+                    };
+
+                    db.Student.Add(newStudent);
+                    db.SaveChanges();
+
+                    BindGrid(db.Student.ToList());
+
+                    MessageBox.Show("Thêm sinh viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi thêm sinh viên: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Model1 db = new Model1()) // Thay StudentContextDB bằng Model1
+                {
+                    // Kiểm tra dữ liệu đầu vào
+                    string studentIDText = txtmssv.Text.Trim();
+                    string fullName = txtHoTen.Text.Trim();
+                    string averageScoreText = txtDiemTb.Text.Trim();
+
+                    if (string.IsNullOrEmpty(studentIDText))
+                    {
+                        MessageBox.Show("Mã sinh viên không được để trống.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!int.TryParse(studentIDText, out int studentID))
+                    {
+                        MessageBox.Show("Mã sinh viên phải là một số nguyên.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (string.IsNullOrEmpty(fullName))
+                    {
+                        MessageBox.Show("Tên sinh viên không được để trống.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!int.TryParse(cmbKhoa.SelectedValue?.ToString(), out int facultyID))
+                    {
+                        MessageBox.Show("Vui lòng chọn một khoa hợp lệ.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    if (!double.TryParse(averageScoreText, out double averageScore))
+                    {
+                        MessageBox.Show("Điểm trung bình không hợp lệ. Vui lòng nhập số.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Tìm sinh viên cần cập nhật
+                    var student = db.Student.FirstOrDefault(s => s.StudentID == studentID);
+                    if (student == null)
+                    {
+                        MessageBox.Show("Không tìm thấy sinh viên với mã đã nhập.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Cập nhật thông tin sinh viên
+                    student.FullName = fullName;
+                    student.FacultyID = facultyID;
+                    student.AverageScore = averageScore;
+
+                    db.SaveChanges();
+
+                    // Cập nhật lại danh sách trên DataGridView
+                    BindGrid(db.Student.ToList());
+
+                    MessageBox.Show("Cập nhật thông tin sinh viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi cập nhật thông tin sinh viên: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Model1 db = new Model1())
+                {
+                    // Lấy mã sinh viên từ ô nhập liệu
+                    string studentIDText = txtmssv.Text.Trim();
+
+                    if (!int.TryParse(studentIDText, out int studentID))
+                    {
+                        MessageBox.Show("Mã sinh viên phải là một số nguyên.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Tìm sinh viên cần xóa
+                    var student = db.Student.SingleOrDefault(s => s.StudentID == studentID);
+
+                    if (student == null)
+                    {
+                        MessageBox.Show("Không tìm thấy sinh viên với mã này.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    // Xác nhận trước khi xóa
+                    var confirmResult = MessageBox.Show("Bạn có chắc chắn muốn xóa sinh viên này không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (confirmResult == DialogResult.Yes)
+                    {
+                        db.Student.Remove(student);
+                        db.SaveChanges();
+
+                        // Cập nhật lại danh sách trên DataGridView
+                        BindGrid(db.Student.ToList());
+
+                        MessageBox.Show("Xóa sinh viên thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi xóa sinh viên: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+ 
+        
+
+        private void dgvSV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (e.RowIndex >= 0)
+            //{
+            //    DataGridViewRow row = dgvSV.Rows[e.RowIndex];
+
+            //    txtmssv.Text = row.Cells[0].Value?.ToString();
+            //    txtHoTen.Text = row.Cells[1].Value?.ToString();
+            //    cmbKhoa.SelectedValue = row.Cells[2].Value;
+            //    txtDiemTb.Text = row.Cells[3].Value?.ToString();
+            //}
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvSV.Rows[e.RowIndex];
+
+                txtmssv.Text = row.Cells[0].Value?.ToString();
+                txtHoTen.Text = row.Cells[1].Value?.ToString();
+
+                // Đặt giá trị cho cmbKhoa dựa trên FacultyID
+                using (Model1 db = new Model1())
+                {
+                    var facultyName = row.Cells[2].Value?.ToString();
+                    var faculty = db.Faculty.FirstOrDefault(f => f.FacultyName == facultyName);
+                    if (faculty != null)
+                    {
+                        cmbKhoa.SelectedValue = faculty.FacultyID;
+                    }
+                }
+
+                txtDiemTb.Text = row.Cells[3].Value?.ToString();
+            }
+        }
+    }
+}
+
+
